29d272ced80306f7c122d6a91e686279813415ba
Anuj Panchal <anujpan1@gmail.com>
2018-09-21 17:07:41 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -22,7 +22,7 @@ def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
   
   cheese_types.each do |cheese|
-    if array.include
+    if array.include?(cheese)
   end
 end
 
